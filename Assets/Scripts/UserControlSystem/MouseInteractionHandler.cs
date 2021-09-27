291d3a2a4a80f24f316eccb086e897389b8cf429
Andrey Zakharov <raven159@yandex.ru>
2021-09-28 01:03:57 +0300

    Add script and modified scene

---
+++ Assets/Scripts/UserControlSystem/MouseInteractionHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core;
+using System.Linq;
+using UnityEngine;
+
+public class MouseInteractionHandler : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+
+    public void Update()
+    {
+        if (!Input.GetMouseButtonUp(0)) return;
+        var hits = Physics.RaycastAll(_camera.ScreenPointToRay(Input.mousePosition));
+        if (hits.Length == 0) return;
+        var mainBuilding = hits
+            .Select(hit => hit.collider.GetComponentInParent<MainBuilding>())
+            .Where(hit => hit != null)
+            .FirstOrDefault();
+        if (mainBuilding == default) return;
+        mainBuilding.ProduceUnit();
+    }
+}

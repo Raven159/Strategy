ac3f4c7e261f013cd88c52b0b6c460cfb7b2b42e
Andrey Zakharov <raven159@yandex.ru>
2021-09-28 23:13:17 +0300

    Add and modified
    Modified:
    Main scene;
    UserControlSystem;
    script MouseInteractionHandler.
    
    Add:
    script BottomLeftUI;
    script SelectableValue.

--- Assets/Scripts/UserControlSystem/MouseInteractionHandler.cs
+++ Assets/Scripts/UserControlSystem/MouseInteractionHandler.cs
@@ -1,21 +1,21 @@
-﻿using Core;
+﻿using Abstractions;
 using System.Linq;
 using UnityEngine;
 
 public class MouseInteractionHandler : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
+    [SerializeField] private SelectableValue _selectedObject;
 
     public void Update()
     {
         if (!Input.GetMouseButtonUp(0)) return;
         var hits = Physics.RaycastAll(_camera.ScreenPointToRay(Input.mousePosition));
         if (hits.Length == 0) return;
-        var mainBuilding = hits
-            .Select(hit => hit.collider.GetComponentInParent<MainBuilding>())
+        var selectable = hits
+            .Select(hit => hit.collider.GetComponentInParent<ISelectable>())
             .Where(hit => hit != null)
             .FirstOrDefault();
-        if (mainBuilding == default) return;
-        mainBuilding.ProduceUnit();
+        _selectedObject.SetValue(selectable);
     }
 }
---
+++ Assets/Scripts/UserControlSystem/SelectableValue.cs
@@ -0,0 +1,16 @@
+﻿using Abstractions;
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(SelectableValue), menuName = "Strategy Game/" + nameof(SelectableValue), order = 0)]
+public class SelectableValue : ScriptableObject
+{
+    public ISelectable CurrentValue { get; private set; }
+    public Action<ISelectable> OnSelectable;
+
+    public void SetValue(ISelectable value)
+    {
+        CurrentValue = value;
+        OnSelectable?.Invoke(value);
+    }
+}

0ba199207e9412dcc498257a4407d4ed3df04231
Andrey Zakharov <raven159@yandex.ru>
2021-10-13 00:12:08 +0300

    Add and modified
    Modified NewValueNotifier scripts,
     add StopAwaiter and AwaiterBase classes

--- Assets/Scripts/Abstractions/NewValueNotifier.cs
+++ Assets/Scripts/Abstractions/NewValueNotifier.cs
@@ -3,12 +3,9 @@ using Utils;
 
 namespace UserControlSystem
 {
-    public class NewValueNotifier<TAwaited> : IAwaiter<TAwaited>
+    public class NewValueNotifier<TAwaited> : AwaiterBase<TAwaited>
     {
         private readonly ScriptableObjectValueBase<TAwaited> _scriptableObjectValueBase;
-        private TAwaited _result;
-        private Action _continuation;
-        private bool _isCompleted;
 
         public NewValueNotifier(ScriptableObjectValueBase<TAwaited> scriptableObjectValueBase)
         {
@@ -19,23 +16,7 @@ namespace UserControlSystem
         private void ONNewValue(TAwaited obj)
         {
             _scriptableObjectValueBase.OnNewValue -= ONNewValue;
-            _result = obj;
-            _isCompleted = true;
-            _continuation?.Invoke();
+            onWaitFinish(obj);
         }
-
-        public void OnCompleted(Action continuation)
-        {
-            if (_isCompleted)
-            {
-                continuation?.Invoke();
-            }
-            else
-            {
-                _continuation = continuation;
-            }
-        }
-        public bool IsCompleted => _isCompleted;
-        public TAwaited GetResult() => _result;
     }
 }
---
+++ Assets/Scripts/Abstractions/StopAwaiter.cs
@@ -0,0 +1,22 @@
+﻿using Core;
+using Utils;
+
+namespace Abstractions
+{
+    public class StopAwaiter : AwaiterBase<AsyncExtensions.Void>
+    {
+        private readonly UnitMovementStop _unitMovementStop;
+
+        public StopAwaiter(UnitMovementStop unitMovementStop)
+        {
+            _unitMovementStop = unitMovementStop;
+            _unitMovementStop.OnStop += onStop;
+        }
+
+        private void onStop()
+        {
+            _unitMovementStop.OnStop -= onStop;
+            onWaitFinish(new AsyncExtensions.Void());
+        }
+    }
+}

f0b44dfdaff7e840878a10e650a3ba928b420ecf
Andrey Zakharov <raven159@yandex.ru>
2021-10-21 22:32:26 +0300

    Add scripts
    Add Scripts from Lesson 5

---
+++ Assets/Scripts/MenuPresenter.cs
@@ -0,0 +1,15 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPresenter : MonoBehaviour
+{
+    [SerializeField] private Button _comeBackButton;
+    [SerializeField] private Button _exitButton;
+
+    private void Start()
+    {
+        _comeBackButton.OnClickAsObservable().Subscribe(_ => gameObject.SetActive(false));
+        _exitButton.OnClickAsObservable().Subscribe(_ => Application.Quit());
+    }
+}
--- Assets/Scripts/TopPanelPresenter.cs
+++ Assets/Scripts/TopPanelPresenter.cs
@@ -3,11 +3,14 @@ using System;
 using TMPro;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 public class TopPanelPresenter : MonoBehaviour
 {
     [SerializeField] private TMP_InputField _inputField;
+    [SerializeField] private Button _menuButton;
+    [SerializeField] private GameObject _menuGameObject;
 
     [Inject]
     private void Init(ITimeModel timeModel)
@@ -16,7 +19,8 @@ public class TopPanelPresenter : MonoBehaviour
         {
             var t = TimeSpan.FromSeconds(seconds);
             _inputField.text = string.Format("{0:D2}:{1:D2}", t.Minutes, t.Seconds);
-
         });
+
+        _menuButton.OnClickAsObservable().Subscribe(_ => _menuGameObject.SetActive(true));
     }
 }
